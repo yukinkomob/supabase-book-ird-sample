1e5a4c941a77698b3afb10d613bb1fe24d284820
yukinkomob <yukinkopen@gmail.com>
2022-01-30 15:34:37 +0900

    prepare to use tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  mode: "jit",
+  purge: ['./src/**/*.(js,ts,jsx,tsx)'],
+  darkMode: false,
   content: [],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
