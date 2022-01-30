0a05a96e9d62d04553275abbce337504c5a8ee77
Yuki Nishimura <yukinkopen@gmail.com>
2022-01-30 17:16:23 +0900

    make base on Manga list

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
+import { Auth } from "@supabase/ui";
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthLayout } from "src/layout/AuthLayout";
+import { client } from "src/libs/supabase";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({Component, pageProps}: AppProps) => {
+  return (
+    <div>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
+      </Auth.UserContextProvider>
+    </div>
+  );
+};
 
-export default MyApp
+export default MyApp;
